c071ea8ddf6dc3f8eb8b563f21da15a573ca8426
Jing Conan Wang <wangjing@bu.edu>
2013-10-31 11:58:37 -0400

    add host and path when displaying the items.

--- build/scripts/view-history.js
+++ build/scripts/view-history.js
@@ -11,28 +11,31 @@ function buildHistoryData(divName) {
             i18n_prompt_delete_button: 'delete',
             i18n_expand_button: 'button',
             i18n_collapse_button: 'collapse',
-            i18n_search_by_domain: 'search',
+            i18n_search_by_domain: 'More for this site',
             i18n_prompt_delete_button: 'prompt_delete',
     };
 
     function doneSearchQuery(historyItems) {
         // FOr each history iterm, get deails
-        //
-        // document.write("run done Search Query");
-        // document.write("historyItems.length: " + historyItems.length);
         var history = []
+        var item, visits, lastVisitDate, bundle;
+        var host, url, pathArray;
         for(var i = 0; i < historyItems.length; ++i) {
-            var item = historyItems[i];
-            var visits = [{
+            item = historyItems[i];
+            pathArray = item.url.split('/');
+            host = pathArray[2] + '/';
+            path = pathArray[3];
+            visits = [{
                 isGrouped: false,
                 url: item.url,
+                domain: host,
                 title: item.title,
-                host: '',
-                path: '',
+                host: host,
+                path: path,
                 id: 'c' + i.toString(),
             }];
-            var lastVisitDate = new Date(item.lastVisitTime);
-            var bundle = {
+            lastVisitDate = new Date(item.lastVisitTime);
+            bundle = {
                 time: lastVisitDate.toISOString(),
                 // time: prettyDate(lastVisitDate.toISOString()),
                 id: '20:20',
