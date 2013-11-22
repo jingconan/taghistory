e2441fd893bf3fbefd57774f13c6342989b96b35
Jing Conan Wang <wangjing@bu.edu>
2013-11-22 11:56:35 -0500

    Improve framework of Evernote Service.

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -34,33 +34,35 @@ TH.Views.updateInterval(TH.Para.Interval.init);
 TH.Views.RefreshButton();
 
 
-function testCreateNote() {
-    var noteStoreURL = 'https://sandbox.evernote.com/shard/s1/notestore';
-    var authenticationToken = 'S=s1:U=87f14:E=1492b56b806:C=141d3a58c0a:P=1cd:A=en-devtoken:V=2:H=cfb7ab84ff2f5a7a6a527657d51fb3f8';
-    var noteStoreTransport = new Thrift.BinaryHttpTransport(noteStoreURL);
-    var noteStoreProtocol = new Thrift.BinaryProtocol(noteStoreTransport);
-    var noteStore = new NoteStoreClient(noteStoreProtocol);
+// function testCreateNote() {
+//     var noteStoreURL = 'https://sandbox.evernote.com/shard/s1/notestore';
+//     var authenticationToken = 'S=s1:U=87f14:E=1492b56b806:C=141d3a58c0a:P=1cd:A=en-devtoken:V=2:H=cfb7ab84ff2f5a7a6a527657d51fb3f8';
+//     var noteStoreTransport = new Thrift.BinaryHttpTransport(noteStoreURL);
+//     var noteStoreProtocol = new Thrift.BinaryProtocol(noteStoreTransport);
+//     var noteStore = new NoteStoreClient(noteStoreProtocol);
 
-    noteStore.listNotebooks(authenticationToken, function (notebooks) {
-        console.log(notebooks);
-    },
-    function onerror(error) {
-        console.log(error);
-    });
+//     noteStore.listNotebooks(authenticationToken, function (notebooks) {
+//         console.log(notebooks);
+//     },
+//     function onerror(error) {
+//         console.log(error);
+//     });
 
-    var note = new Note();
-    note.title = "Test note from EDAMTest.js";
-    note.content = '<?xml version="1.0" encoding="UTF-8"?>';
-    note.content += '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">';
-    note.content += '<en-note>Here is the Evernote logo: Test by Jing Wang<br/>';
-    note.content += '</en-note>';
+//     var note = new Note();
+//     note.title = "Test note from EDAMTest.js";
+//     note.content = '<?xml version="1.0" encoding="UTF-8"?>';
+//     note.content += '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">';
+//     note.content += '<en-note>Here is the Evernote logo: Test by Jing Wang<br/>';
+//     note.content += '</en-note>';
 
-    noteStore.createNote(authenticationToken, note, function(err, createdNote) {
-        console.log();
-        console.log("Creating a new note in the default notebook");
-        console.log();
-        console.log("Successfully created a new note with GUID: ");
-    });
-}
+//     noteStore.createNote(authenticationToken, note, function(err, createdNote) {
+//         console.log();
+//         console.log("Creating a new note in the default notebook");
+//         console.log();
+//         console.log("Successfully created a new note with GUID: ");
+//     });
+// }
 
-testCreateNote();
+// testCreateNote();
+
+$('#sync_to_evernote').on("click", TH.Services.Evernote.sync);
--- build/scripts/services.js
+++ build/scripts/services.js
@@ -12,33 +12,53 @@ var Note = Note || {};
 TH.Services.Evernote.init = function(_) {
 };
 
+TH.Services.Evernote.format = function(info) {
+    // inptut is the info from the chrome storage
+    // output is the formatted note string
+    // FIXME this is a stub, need to be finished
+    // [2013-11-22 11:55:51]
+    return "TH.Services.Evernote.format has been executed";
+};
+
 TH.Services.Evernote.sync = function(_) {
-    console.log("run here");
-    var noteStoreURL = TH.Services.Evernote.noteStoreURL;
-    var authenticationToken = TH.Services.Evernote.authenticationToken;
-    var noteStoreTransport = new Thrift.BinaryHttpTransport(noteStoreURL);
-    var noteStoreProtocol = new Thrift.BinaryProtocol(noteStoreTransport);
-    var noteStore = new NoteStoreClient(noteStoreProtocol);
+    var oneWeekAgo = (new Date()).getTime() - TH.Para.query_time;
+    var searchQuery = {
+        'text': '',
+        'startTime': oneWeekAgo,
+    };
 
-    noteStore.listNotebooks(authenticationToken, function (notebooks) {
-        console.log(notebooks);
-    },
-    function onerror(error) {
-        console.log(error);
-    });
 
-    var note = new Note();
-    note.title = "Test note from EDAMTest.js";
-    note.content = '<?xml version="1.0" encoding="UTF-8"?>';
-    note.content += '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">';
-    note.content += '<en-note>Here is the Evernote logo: Test by Jing Wang<br/>';
-    note.content += '</en-note>';
+    TH.Models.fetchAllData(searchQuery, function(storedInfo) {
+        console.log("run here");
+        var noteStoreURL = TH.Services.Evernote.noteStoreURL;
+        var authenticationToken = TH.Services.Evernote.authenticationToken;
+        var noteStoreTransport = new Thrift.BinaryHttpTransport(noteStoreURL);
+        var noteStoreProtocol = new Thrift.BinaryProtocol(noteStoreTransport);
+        var noteStore = new NoteStoreClient(noteStoreProtocol);
 
-    noteStore.createNote(authenticationToken, note, function(err, createdNote) {
-        console.log();
-        console.log("Creating a new note in the default notebook");
-        console.log();
-        console.log("Successfully created a new note with GUID: ");
+        noteStore.listNotebooks(authenticationToken, function (notebooks) {
+            console.log(notebooks);
+        },
+        function onerror(error) {
+            console.log(error);
+        });
+
+        var note = new Note();
+        note.title = "Test note from EDAMTest.js";
+        note.content = '<?xml version="1.0" encoding="UTF-8"?>';
+        note.content += '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">';
+        note.content += '<en-note>Here is the Evernote logo: Test by Jing Wang<br/>';
+        note.content += TH.Services.Evernote.format(storedInfo);
+        note.content += '</en-note>';
+
+        noteStore.createNote(authenticationToken, note, function(err, createdNote) {
+            console.log();
+            console.log("Creating a new note in the default notebook");
+            console.log();
+            console.log("Successfully created a new note with GUID: ");
+        });
+
+
     });
 };
 
