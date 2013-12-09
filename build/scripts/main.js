8aa677ec1270f756e1f4be724a0365146fc94e87
wangjing <wangjing AT bu.edu>
2013-12-08 20:08:34 -0500

    refactor main.js

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -1,29 +1,59 @@
-/*jslint browser: true*/
+/*jslint browser: true, vars:true*/
 /*global $, TH*/
 "use strict";
-TH.Models.init(TH);
-var TH_interval = TH.Para.Interval;
-var TH_views = TH.Views;
+var main = function (TH) {
+    TH.Models.init(TH);
+    var TH_interval = TH.Para.Interval;
+    var TH_views = TH.Views;
+    // var $ = TH.Modules.$;
 
-/*jslint unparam: true*/
-$(TH.Selectors.interval_slider).slider({
-    value: TH_interval.init,
-    min: TH_interval.min,
-    max: TH_interval.max,
-    step: TH_interval.step,
-    slide: function (event, ui) {
-        console.log('moved'); // Run code if slider value changes
-        TH_views.updateInterval(ui.value);
-    },
-    stop: function (event, ui) {
-        console.log('released handle: ' + ui.value);
-        var interval = TH_views.intervalValue(),
-            massageInfo = TH.Models.divideData(TH.Store.storedInfo, interval);
-        TH_views.renderHistory(massageInfo);
+    /*jslint unparam: true*/
+    $(TH.Selectors.interval_slider).slider({
+        value: TH_interval.init,
+        min: TH_interval.min,
+        max: TH_interval.max,
+        step: TH_interval.step,
+        slide: function (event, ui) {
+            console.log('moved'); // Run code if slider value changes
+            TH_views.updateInterval(ui.value);
+        },
+        stop: function (event, ui) {
+            console.log('released handle: ' + ui.value);
+            var interval = TH_views.intervalValue();
+            var massageInfo = TH.Models.divideData(TH.Store.storedInfo, interval);
+            TH_views.renderHistory(massageInfo);
+        }
+    });
+    /*jslint unparam: false*/
+
+    TH_views.updateInterval(TH.Para.Interval.init);
+    TH_views.RefreshButton();
+    $('#sync_to_evernote').on("click", TH.Services.Evernote.sync);
+};
+
+var update = function (dst, src) {
+    var mks = Object.keys(src);
+    var i = 0, N = mks.length;
+    for (i = 0; i < N; i += 1) {
+        dst[mks[i]] = src[mks[i]];
     }
-});
-/*jslint unparam: false*/
+};
 
-TH_views.updateInterval(TH.Para.Interval.init);
-TH_views.RefreshButton();
-$('#sync_to_evernote').on("click", TH.Services.Evernote.sync);
+main(TH);
+
+// var loadModules = function (names, callback) {
+//     require(names, function () {
+//         var modules = {};
+//         var i = 0, N = names.length;
+//         for (i = 0; i < N; i += 1) {
+//             modules[names[i]] = arguments[i];
+//         }
+//         callback(modules);
+//     });
+// };
+
+// loadModules(['jquery', 'jquery-ui', 'mustache', 'evernote-sdk-minified'], function (modules) {
+// loadModules(['mustache-wrap'], function (modules) {
+    // update(TH.Modules, modules);
+    // main(TH);
+// });
