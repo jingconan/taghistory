b5213df2613167185a69bb21e0c016ba79d20b26
Jing Conan Wang <hbhzwj@gmail.com>
2014-08-01 10:23:34 -0400

    Remove comments in collections.js

--- build/scripts/collections.js
+++ build/scripts/collections.js
@@ -30,60 +30,12 @@ _.extend(Backbone.Collection.prototype, {
 
 TH.Collections.Intervals = Backbone.Collection.extend({
     model: TH.Models.Interval,
-    // toTemplate: function () {
-    //     var intervals = [];
-    //     var i;
-    //     for (i = 0; i < this.models.length; ++i) {
-    //         intervals.push(this.models[i].toTemplate);
-    //     }
-    //     return intervals;
-    // },Array[0]
-
-    destroyAll: function () {
-        while (this.length > 0) {
-            if (this.at(0)) {
-                this.at(0).destroy();
-            }
-        }
-    }
 });
-
-
-function lazyPersistence() {
-    return new TH.Persistence.Tag({localStore: chrome.storage.sync});
-}
 
 TH.Collections.Tags = Backbone.Collection.extend({
     model: TH.Models.Tag,
     modelName: 'tagList',
-    chromeStorage: new Backbone.ChromeStorage("Tags", "local"),
-    // constructor : function (attributes, options) {
-        // Backbone.Collection.apply( this, arguments );
-    // },
-    // initialize: function (models, options) {
-    //     this.persistence = options.persistence || lazyPersistence();
-    // },
-    // fetch: function (callback) {
-    //     console.log('TH.Collections.Tags.fetch');
-    //     this.persistence = this.persistence || lazyPersistence();
-    //     this.persistence.fetchTags((function (tags, compileTags) {
-    //         this.models = tags;
-    //         callback();
-    //     }).bind(this));
-    // },
-    // destroy: function (callback) {
-    //     this.persistence = this.persistence || lazyPersistence();
-    //     this.persistence.removeAllTags(callback);
-    // }
-    // toTemplate: function () {
-    //     var tagList = [];
-    //     var i;
-    //     for (i = 0; i < this.models.length; ++i) {
-    //         tagList.push({tag_name: this.models[i]});
-    //     }
-    //     return {tagList: tagList};
-    // }
-    
+    chromeStorage: new Backbone.ChromeStorage("Tags", "local")
 });
 
 TH.Collections.Intervals = Backbone.Collection.extend({
